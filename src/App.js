c662f317ae27296f2a235c0808c0dcbd3962e7f9
navir-rahman <navirrahman@gmail.com>
2022-04-17 13:10:57 +0600

    heaver added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Sheared/Header/Header';
@@ -7,6 +7,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Pages/Home/Home';
 import SignUp from './Components/Pages/SignUp/SignUp';
 import SignIn from './Components/Pages/SignIn/SignIn';
+import Services from './Components/Pages/Services/Services';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/services' element={<Services></Services>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
         <Route path='/signin' element={<SignIn></SignIn>}></Route>
       </Routes>
--- src/Components/Sheared/Header/Header.js
+++ src/Components/Sheared/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../../../logo.jpg';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -7,11 +8,12 @@ const Header = () => {
         <div>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand as={Link} to={"/"}><img src={logo} alt="" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to={"/home"}>Home</Nav.Link>
+                            <Nav.Link as={Link} to={"/services"}>Our Services</Nav.Link>
                             <Nav.Link as={Link} to={"/signup"}>sign up</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
